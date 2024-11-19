488e41aa7a27c504a3af38a0cd060df24e5099f2
XcaLiBuR75 <ghernan75@gmail.com>
2024-11-19 00:17:45 -0300

    Update 2024-11-19 00:17         Linux/x86_64-G5-ARCH-B

---
+++ .config/nvim/lua/plugins/core.lua
@@ -0,0 +1,23 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "rose-pine/neovim", name = "rose-pine" },
+  { "neanias/everforest-nvim", priority = 1000 },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+
+  -- change catppuccin config
+  {
+    "catppuccin/nvim",
+    -- opts will be merged with the parent spec
+    opts = { transparent_background = true },
+  },
+}
---
+++ .config/zsh/.zprofile
@@ -0,0 +1,3 @@
+
+# Created by `pipx` on 2024-06-16 01:22:07
+export PATH="$PATH:/home/zerohack/.local/bin"
