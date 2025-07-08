558cc1c0e5aa5f13d3b23b36c831d05c4f39f32f
XcaLiBuR75 <ghernan75@gmail.com>
2025-07-08 09:05:36 -0300

    Update 2025-07-08 09:05         Linux/x86_64-G5-ARCH-B

--- .local/bin/cycle_wall.sh
+++ .local/bin/cycle_wall.sh
@@ -5,4 +5,4 @@ IMG_NAME=$(ls $HOME/Pictures/walls | shuf -n 1)
 # full path to that random image file
 IMG_PATH=$(find -L "$HOME/Pictures" -type f | shuf -n 1)
 # Command to change wallpaper
-swww img $IMG_PATH --transition-type random --transition-fps 144 --transition-step 5
+swww img $IMG_PATH --transition-type wave --transition-fps 144 --transition-step 2
