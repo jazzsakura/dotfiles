93d67653a4c9be0376a598bec284cf48fd2c3790
XcaLiBuR75 <ghernan75@gmail.com>
2025-08-10 00:50:06 -0300

    Update 2025-08-10 00:50         Linux/x86_64-G5-ARCH-B

--- .config/nvim/lua/core/remap.lua
+++
@@ -1,8 +0,0 @@
-vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>p', '"+p')
