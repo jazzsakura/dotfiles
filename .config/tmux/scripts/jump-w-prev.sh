89d7a550c0ac4c94ac8c3df0cb226d86d00ce329
XcaLiBuR75 <ghernan75@gmail.com>
2026-02-12 00:37:28 -0300

    Update 2026-02-12 00:37         Linux/x86_64-G5-ARCH-B

--- .config/tmux/scripts/jump-w-prev.sh
+++ .config/tmux/scripts/jump-w-prev.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+session="$(tmux list-sessions | grep -i attached | cut -d ":" -f1)"
+
 # Popup Selector
-tmux list-windows -F '#D' |\
+#tmux list-windows -F '#D' |\
+tmux list-windows -F '#W' |\
 sed '$d' |\
-fzf --tmux 65% --header 'Jump to window: ' --preview 'tmux capture-pane -e -pt {}'  |\
-xargs tmux switch-client -t
+fzf --tmux 65% --header 'Jump to window: ' --preview 'tmux capture-pane -e -pt {}' |\
+xargs -I{} tmux switch-client -t$session:{}
