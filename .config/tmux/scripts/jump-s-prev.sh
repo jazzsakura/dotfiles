11d9a21c4885c2a2e92d847ccf6b2826639ab30e
XcaLiBuR75 <ghernan75@gmail.com>
2026-02-19 23:10:39 -0300

    Update 2026-02-19 23:10         Linux/x86_64-G5-ARCH-B

--- .config/tmux/scripts/jump-s-prev.sh
+++ .config/tmux/scripts/jump-s-prev.sh
@@ -3,5 +3,5 @@
 # Popup Selector
 tmux list-sessions -F '#{?session_attached,,#{session_name}}' |\
 sed '/^$/d' |\
-fzf --tmux 70% --header 'Jump to session: ' --preview 'tmux capture-pane -e -pt {}' --preview-window=70%  |\
+fzf --tmux 70% --header 'Jump to session: ' --preview 'tmux capture-pane -e -pt {}' --preview-window=75%  |\
 xargs -I{} tmux switch-client -t{}
--- .config/tmux/scripts/jump-w-prev.sh
+++ .config/tmux/scripts/jump-w-prev.sh
@@ -5,5 +5,5 @@ session="$(tmux list-sessions | grep -i attached | cut -d ":" -f1)"
 # Popup Selector
 tmux list-windows -F '#{?#{window_active},,#{window_name}}' |\
 sed '/^$/d' |\
-fzf --tmux 70% --header 'Jump to window: ' --preview 'tmux capture-pane -e -pt {}' --preview-window=70% |\
+fzf --tmux 70% --header 'Jump to window: ' --preview 'tmux capture-pane -e -pt {}' --preview-window=75% |\
 xargs -I{} tmux switch-client -t$session:{}
