3ae46fc40018d06cfc40f9a9a6e929825e28808a
XcaLiBuR75 <ghernan75@gmail.com>
2026-03-24 17:12:24 -0300

    Update 2026-03-24 17:12         Linux/x86_64-G5-ARCH-B

--- .local/bin/updatedb-dir.sh
+++ .local/bin/updatedb-dir.sh
@@ -25,7 +25,6 @@ current_dir="$(echo $PWD | sed 's/^.//')"
 #command grep -ia "^$(printf $current_dir)" $HOME/Downloads/bulk-tmp-dir 2>/dev/null > testfile
 #current="$(command grep -ia "^$(printf $current_dir)" $HOME/Downloads/bulk-tmp-dir 2>/dev/null)"
 command grep -ia "^$(echo $current_dir)" $HOME/Downloads/bulk-tmp-dir 2>/dev/null > file1
-#new="$(command ag -i --hidden --ignore '.gitignore' --ignore-dir '.*git*' -g '' 2>/dev/null | sed "s@^@${PWD}/@" | sed 's/^\///' | sed 's#/[^/]*$##' | LC_ALL=c sort -u)"
 command ag -i --hidden --ignore '.gitignore' --ignore-dir '.*git*' -g '' 2>/dev/null | sed "s@^@${PWD}/@" | sed 's/^\///' | sed 's#/[^/]*$##' | LC_ALL=c sort -u > file2
 
 awk '
@@ -51,7 +50,7 @@ then
   print_info "No new entries found..."
 else
   print_updated "Up to date"
-  awk -i inplace 'NR==FNR{a[$1]; next} !($NF in a)' file1 $HOME/Downloads/bulk-tmp-dir
+  #awk -i inplace 'NR==FNR{a[$1]; next} !($NF in a)' file1 $HOME/Downloads/bulk-tmp-dir
   awk 'NR==FNR {seen[$0];next} !($0 in seen) { seen[$0];print }' file1 file2 > file3  
   cat file3 >> $HOME/Downloads/bulk-tmp-dir
   LC_ALL=c sort -o $HOME/Downloads/bulk-tmp-dir $HOME/Downloads/bulk-tmp-dir
