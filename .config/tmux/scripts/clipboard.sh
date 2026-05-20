28d2e2bae422217c95fe77c30d125a13424134db
XcaLiBuR75 <ghernan75@gmail.com>
2026-05-19 22:00:32 -0300

    Update 2026-05-19 22:00         Linux/x86_64-G5-ARCH-B

---
+++ .config/tmux/scripts/clipboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cliphist list | fzf --tmux 85% \
+  --prompt=" Search clipboard: " \
+  --reverse \
+  --preview 'echo {} | cliphist decode 2>/dev/null' \
+  --preview-window=right:40%:wrap \
+  --header ' Enter: copy | Ctrl+D: delete | Ctrl+A: clear all | Esc: quit' \
+  --bind 'ctrl-d:execute(echo {} | cliphist delete)+reload(cliphist list)' \
+  --bind 'ctrl-a:execute(cliphist wipe)+reload(cliphist list)' \
+  | cliphist decode | wl-copy
---
+++ .local/bin/launcher_toggle.sh
@@ -0,0 +1,1 @@
+pkill -f launcher.sh || footclient --override="colors-dark.alpha=0.65" --app-id=launcher -e bash $HOME/.local/bin/launcher.sh
