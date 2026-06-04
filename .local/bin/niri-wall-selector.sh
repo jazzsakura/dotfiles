44507cff54c95abccc5183f147d7466900207e8d
XcaLiBuR75 <ghernan75@gmail.com>
2026-06-04 13:19:30 -0300

    Update 2026-06-04 13:19         Linux/x86_64-G5-ARCH-B

---
+++ .local/bin/niri-wall-selector.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+wallDir="${HOME}/Pictures/wallhaven"
+export wallSet="${HOME}/.config/niri/wall.jpg"
+export homeDir="${PWD}"
+
+IMG=$(rg --color never -L -u --hidden --no-config --files --glob '!.*git*' --glob '!.npm*' $wallDir | 
+sed -nE '/.*\.(jpg|jpeg|png|gif|bmp|mp4)$/Ip'  |
+fzf --preview 'kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no {}' --preview-window 'up,99%,border-none,noinfo' --reverse --header-first --inline-info --prompt='' --no-sort --no-input --info='hidden')
+awww img "$IMG" --transition-type any --transition-step 5 --transition-fps 165 --transition-duration=2
+ln -fs "${IMG}" "${wallSet}"
