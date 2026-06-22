c0946c3f053c4bc9488a10b8ea1a1476f4a6581e
XcaLiBuR75 <ghernan75@gmail.com>
2026-06-22 13:34:14 -0300

    Update 2026-06-22 13:34         Linux/x86_64-G5-ARCH-B

--- .local/bin/niri-wall-selector.sh
+++ .local/bin/niri-wall-selector.sh
@@ -10,5 +10,7 @@ shuf |
 sed -nE '/.*\.(jpg|jpeg|png|gif|bmp|mp4)$/Ip' |
 fzf --bind 'j:down,k:up' --preview 'kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no {}' --preview-window 'up,99%,border-none,noinfo' --reverse --header-first --inline-info --prompt='' --no-sort --no-input --info='hidden')
 pgrep -f mpvpaper && pkill mpvpaper
-awww img "$IMG" --transition-type any --transition-step 5 --transition-fps 165 --transition-duration=2
+awww img "$IMG" --transition-type any --transition-step 2 --transition-fps 165 --transition-duration=1.6 --transition-bezier 0.33,1.0,0.68,1.0
+#awww img "$IMG" --transition-type any --transition-step 5 --transition-fps 165 --transition-duration=2
+notify-send "Wallpaper changed"  "$(basename "$IMG")" -i "$IMG"
 ln -fs "${IMG}" "${wallSet}"
