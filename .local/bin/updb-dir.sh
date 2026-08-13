198607f82138fe3a4c5af5c158fefc39818832f9
XcaLiBuR75 <ghernan75@gmail.com>
2026-08-13 08:08:07 -0300

    Update 2026-08-13 08:08 \ Linux/x86_64-G5-ARCH-B

--- .local/bin/updb-dir.sh
+++ .local/bin/updb-dir.sh
@@ -22,20 +22,8 @@ print_success() {
 }
 
 current_dir="$(echo $PWD | sed 's/^.//')"
-#command grep -ia "^$(printf $current_dir)" $HOME/Downloads/dirs-db 2>/dev/null > testfile
-#current="$(command grep -ia "^$(printf $current_dir)" $HOME/Downloads/dirs-db 2>/dev/null)"
 command grep -ia "^$(echo $current_dir)" $HOME/Downloads/dirs-db 2>/dev/null > /tmp/file1
 command ag -f -i --hidden --ignore '.gitignore' --ignore-dir '.*git*' -g '' 2>/dev/null | sed "s@^@${PWD}/@" | sed 's/^\///' | sed 's#/[^/]*$##' | LC_ALL=c sort -u > /tmp/file2
-
-awk '
-NR==FNR {
-    seen[$0]
-    next
-}
-!($0 in seen) {              # if word is not hashed to seen
-    seen[$0]                 # hash unseen a.txt words to seen to avoid duplicates 
-    print                    # and output it
-}' /tmp/file1 /tmp/file2 > /tmp/file3
 
 if [ -e "/tmp/file1" ] && [ ! -s "/tmp/file1" ]; then
   print_success "NEW entries found!..."
@@ -51,10 +39,10 @@ then
   print_info "No new entries found..."
 else
   print_updated "Up to date"
-  awk -i inplace 'NR==FNR{a[$1]; next} !($NF in a)' /tmp/file1 $HOME/Downloads/dirs-db
-  awk 'NR==FNR {seen[$0];next} !($0 in seen) { seen[$0];print }' $HOME/Downloads/dirs-db /tmp/file3 > /tmp/file4  
-  cat /tmp/file2 >> /tmp/file4
-  cat /tmp/file4 >> $HOME/Downloads/dirs-db
-  LC_ALL=c sort -u -o $HOME/Downloads/dirs-db $HOME/Downloads/dirs-db
+  #grep -Fvxf /tmp/file1 $HOME/Downloads/dirs-db > /tmp/cleaned.txt
+  awk 'NR==FNR{a[$0];next} !($0 in a)' /tmp/file1 $HOME/Downloads/dirs-db > /tmp/cleaned.txt
+  cat /tmp/file2 >> /tmp/cleaned.txt
+  cat /tmp/cleaned.txt > $HOME/Downloads/dirs-db
+  LC_ALL=C sort -u -o $HOME/Downloads/dirs-db $HOME/Downloads/dirs-db
 fi
-rm -vf /tmp/file* &>/dev/null
+rm -vf /tmp/file* /tmp/cleaned.txt &>/dev/null
